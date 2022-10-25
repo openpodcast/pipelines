e9d5cc39a5e78463dbecb9782762a5f75d9f719a
Matthias <matthias-endler@gmx.net>
2022-10-25 18:50:27 +0200

    Fix typos

--- spotify/main.py
+++ spotify/main.py
@@ -10,10 +10,11 @@ CLIENT_ID = "05a1371ee5194c27860b3ff3ff3979d2"
 SPOTIFY_PODCAST_ID = os.environ.get("SPOTIFY_PODCAST_ID")
 SP_DC = os.environ.get("SPOTIFY_SP_DC")
 SP_KEY = os.environ.get("SPOTIFY_SP_KEY")
-FEED_URL = 'https://feeds.redcircle.com/2c2cd740-1c1f-4928-adac-98a692dbf4c2'
+FEED_URL = "https://feeds.redcircle.com/2c2cd740-1c1f-4928-adac-98a692dbf4c2"
 OPENPODCAST_API_ENDPOINT = "https://api.openpodcast.dev/connector"
 # OPENPODCAST_API_ENDPOINT = "http://localhost:8080/connector"
 OPENPODCAST_API_TOKEN = os.environ.get("OPENPODCAST_API_TOKEN")
+
 
 class OpenPodcastApi:
     def __init__(self, endpoint, token):
@@ -21,13 +22,11 @@ class OpenPodcastApi:
         self.token = token
         pass
 
-    def capture(self, data, range, meta = {}):
-        """ 
+    def capture(self, data, range, meta={}):
+        """
         Send POST request to Open Podcast API.
         """
-        headers = {
-            'Authorization': f'Bearer {self.token}'
-        }
+        headers = {"Authorization": f"Bearer {self.token}"}
         json = {
             "provider": "spotify",
             "version": 1,
@@ -37,8 +36,17 @@ class OpenPodcastApi:
             "data": data,
         }
         return requests.post(self.endpoint, headers=headers, json=json)
-        
-def fetch_and_capture(endpoint_name, file_path_prefix, connector_call, open_podcast_client, start, end, extra_meta = {}):
+
+
+def fetch_and_capture(
+    endpoint_name,
+    file_path_prefix,
+    connector_call,
+    open_podcast_client,
+    start,
+    end,
+    extra_meta={},
+):
     """
     Wrapper function to fetch data from Spotify and directly send to Open Podcast API.
     """
@@ -53,20 +61,25 @@ def fetch_and_capture(endpoint_name, file_path_prefix, connector_call, open_podc
     with open(f"{file_path_prefix}-{dt.datetime.now()}.json", "w+") as f:
         json.dump(data, f)
 
-    result = open_podcast_client.capture(data, 
+    result = open_podcast_client.capture(
+        data,
         # Merge in extra metadata (if any)
-        meta = {**extra_meta, **{
-            "show": SPOTIFY_PODCAST_ID,
-            "endpoint": endpoint_name, 
-        }},
-        range = {
+        meta={
+            **extra_meta,
+            **{
+                "show": SPOTIFY_PODCAST_ID,
+                "endpoint": endpoint_name,
+            },
+        },
+        range={
             "start": start.strftime("%Y-%m-%d"),
             "end": end.strftime("%Y-%m-%d"),
-        }
+        },
     )
     logger.info(f"{endpoint_name}: {result.text}")
 
     return data
+
 
 def main():
     spotify_connector = SpotifyConnector(
@@ -84,28 +97,74 @@ def main():
     start = dt.datetime.now() - dt.timedelta(days=1)
     end = dt.datetime.now()
 
-    fetch_and_capture("metadata", "metadata", lambda: spotify_connector.metadata(), open_podcast_client, start, end)
+    fetch_and_capture(
+        "metadata",
+        "metadata/",
+        lambda: spotify_connector.metadata(),
+        open_podcast_client,
+        start,
+        end,
+    )
 
-    episodes = fetch_and_capture("episodes", "metadata", lambda: spotify_connector.episodes(start, end), open_podcast_client, start, end)
+    episodes = fetch_and_capture(
+        "episodes",
+        "episodes/",
+        lambda: spotify_connector.episodes(start, end),
+        open_podcast_client,
+        start,
+        end,
+    )
 
     for episode in episodes["episodes"]:
-        id = episode['id']
+        id = episode["id"]
 
         # TODO: Get episode metadata. Not implemented by Spotify connector yet.
         # fetch_and_capture("episode_metadata", f"episode_metadata/{id}", lambda: spotify_connector.episode_metadata(id, start, end), open_podcast_client, start, end)
 
-        fetch_and_capture("streams", f"streams/{id}", lambda: spotify_connector.streams(id, start, end), open_podcast_client, start, end, extra_meta={
-            "episode": id,
-        })
-        fetch_and_capture("listeners", f"listeners/{id}", lambda: spotify_connector.listeners(id, start, end), open_podcast_client, start, end, extra_meta={
-            "episode": id,
-        })
-        fetch_and_capture("performance", f"performance/{id}", lambda: spotify_connector.performance(id), open_podcast_client, start, end, extra_meta={
-            "episode": id,
-        })
-        fetch_and_capture("aggregate", f"aggregate/{id}", lambda: spotify_connector.aggregate(id, start, end), open_podcast_client, start, end, extra_meta={
-            "episode": id,
-        })
+        fetch_and_capture(
+            "streams",
+            f"streams/{id}",
+            lambda: spotify_connector.streams(id, start, end),
+            open_podcast_client,
+            start,
+            end,
+            extra_meta={
+                "episode": id,
+            },
+        )
+        fetch_and_capture(
+            "listeners",
+            f"listeners/{id}",
+            lambda: spotify_connector.listeners(id, start, end),
+            open_podcast_client,
+            start,
+            end,
+            extra_meta={
+                "episode": id,
+            },
+        )
+        fetch_and_capture(
+            "performance",
+            f"performance/{id}",
+            lambda: spotify_connector.performance(id),
+            open_podcast_client,
+            start,
+            end,
+            extra_meta={
+                "episode": id,
+            },
+        )
+        fetch_and_capture(
+            "aggregate",
+            f"aggregate/{id}",
+            lambda: spotify_connector.aggregate(id, start, end),
+            open_podcast_client,
+            start,
+            end,
+            extra_meta={
+                "episode": id,
+            },
+        )
 
 
 if __name__ == "__main__":
