5c0efd516e5e180510bb82a8b3edae5170a8d7d6
Matthias <matthias-endler@gmx.net>
2022-12-05 18:14:48 +0100

    Add Spotify Docker image (#14)
    The image runs as a cronjob and fetches data once a day by default

---
+++ spotify/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CRON_FILE=/etc/cron.d/run
+mkdir -p /etc/cron.d
+
+CRON_PATTERN=${CRON_SCHEDULE:-"0 10 * * *"}
+
+echo -e "${CRON_PATTERN} root \
+python /app/main.py > /proc/1/fd/1 2>/proc/1/fd/2 \n" > ${CRON_FILE}
+
+printenv | cat - ${CRON_FILE} > /tmp/cron
+cat /tmp/cron > ${CRON_FILE}
+chmod 0644 ${CRON_FILE}
+
+echo "starting cron (${CRON_PATTERN})"
+cron -f
