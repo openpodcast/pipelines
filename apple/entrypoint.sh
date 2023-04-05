46ed4a974549cfbfa0d77b5d987eaecb4ed01a9e
Wolfgang Gassler <wolfgang@gassler.org>
2023-04-05 12:57:42 +0200

    fix #48 as cron command was not executed

--- apple/entrypoint.sh
+++ apple/entrypoint.sh
@@ -5,9 +5,7 @@ mkdir -p /etc/cron.d
 
 CRON_PATTERN=${CRON_SCHEDULE:-"0 10 * * *"}
 
-echo -e "${CRON_PATTERN} root \
-cd /app 
-python -m job > /proc/1/fd/1 2>/proc/1/fd/2 \n" > ${CRON_FILE}
+echo -e "${CRON_PATTERN} root cd /app && python -m job > /proc/1/fd/1 2>/proc/1/fd/2 \n" > ${CRON_FILE}
 
 printenv | cat - ${CRON_FILE} > /tmp/cron
 cat /tmp/cron > ${CRON_FILE}
