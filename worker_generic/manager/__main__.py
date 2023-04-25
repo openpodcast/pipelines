a42e7a1f6693cb451932a1846477d88aca8a293d
Wolfgang Gassler <wolfgang@gassler.org>
2023-04-25 19:48:58 +0200

    init draft to fetch creds from db and start connector jobs

---
+++ worker_generic/manager/__main__.py
@@ -0,0 +1,59 @@
+from manager.load_env import load_env
+from manager.load_env import load_file_or_env
+import mysql.connector
+from manager.cryptography import decrypt_json
+import os
+import subprocess
+
+print("Initializing worker environment")
+
+CONNECTORS_PATH = load_env("CONNECTORS_PATH", "./../")
+
+MYSQL_HOST = load_env("MYSQL_HOST", "localhost")
+MYSQL_PORT = load_env("MYSQL_PORT", 3306)
+MYSQL_USER = load_env("MYSQL_USER", "root")
+MYSQL_PASSWORD = load_file_or_env("MYSQL_PASSWORD")
+MYSQL_DATABASE = load_env("MYSQL_DATABASE", "openpodcast_auth")
+
+OPENPODCAST_ENCRYPTION_KEY = load_file_or_env("OPENPODCAST_ENCRYPTION_KEY")
+if not OPENPODCAST_ENCRYPTION_KEY:
+    print("No OPENPODCAST_ENCRYPTION_KEY found")
+    exit(1)    
+
+# try to connect to mysql or exit otherwise
+try:
+    db = mysql.connector.connect(
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        user=MYSQL_USER,
+        passwd=MYSQL_PASSWORD,
+        database=MYSQL_DATABASE,
+    )
+except Exception as e:
+    print("Error connecting to mysql: ", e)
+    exit(1)
+
+print("Fetching all podcast tasts from database...")
+
+sql = """
+  SELECT account_id, source_name, source_podcast_id, source_access_keys_encrypted, pod_name
+  FROM podcastSources JOIN openpodcast.podcasts USING (account_id)
+"""
+cursor = db.cursor()
+cursor.execute(sql)
+results = cursor.fetchall()
+
+for result in results:
+  podcast_id = result[0] # the account id of the podcast
+  source_name = result[1] # e.g. spotify or apple
+  source_podcast_id = result[2] # e.g. the podcast id of spotify or apple
+  source_access_keys = decrypt_json(result[3], OPENPODCAST_ENCRYPTION_KEY) # all keys that are needed to access the source
+  pod_name = result[4] # the name of the podcast
+  print("Starting fetcher for", pod_name, podcast_id , "for", source_name, "using podcast_id", source_podcast_id)
+
+  # parent path of fetcher/connector
+  cwd=os.path.join(CONNECTORS_PATH, source_name)
+
+  # run an external process, switch to right fetcher depending on source_name, and set env variables from source_access_keys  
+  subprocess.run(["python","-m","job"], cwd=cwd, env={**os.environ, **source_access_keys})
+
---
+++ worker_generic/manager/cryptography.py
@@ -0,0 +1,21 @@
+import gnupg
+import base64
+import json
+
+gpg = gnupg.GPG()
+
+'''
+Decrypts a json string
+The json object keys are plain text, the value is gpg encrypted and base64 encoded
+e.g. {"key1": "base64encodedgpgencryptedvalue1", "key2": "base64encodedgpgencryptedvalue2"}
+the method supports only one level of nesting, deeper nesting is not supported yet
+'''
+def decrypt_json(json_encrypted, key):
+    dict_encrypted = json.loads(json_encrypted)
+    for k in dict_encrypted:
+        encrypted_binary = base64.b64decode(dict_encrypted[k])
+        try:
+            dict_encrypted[k] = gpg.decrypt(encrypted_binary, passphrase=key).data.decode("utf-8")
+        except Exception as e:
+            print("Error decrypting element: ", k)
+    return dict_encrypted
