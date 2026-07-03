45fc79d55dda41dddb48177334ed16f48834ac85
Wolfgang Gassler <wolfgang@gassler.org>
2026-07-03 12:32:07 +0200

    Pass podcast id when fetching Apple cookies (#139)
    * Pass podcast id when fetching Apple cookies
    
    * Remove Apple cookie helper test
    
    * formatting
    
    ---------
    
    Co-authored-by: Matthias Endler <matthias@endler.dev>

--- apple/job/__main__.py
+++ apple/job/__main__.py
@@ -125,7 +125,9 @@ if response.status_code != 200:
 logger.info(
     f"Receiving cookies from Apple from automation endpoint {APPLE_AUTOMATION_ENDPOINT}"
 )
-cookies = apple.get_cookies(APPLE_AUTOMATION_BEARER_TOKEN, APPLE_AUTOMATION_ENDPOINT)
+cookies = apple.get_cookies(
+    APPLE_AUTOMATION_BEARER_TOKEN, APPLE_AUTOMATION_ENDPOINT, APPLE_PODCAST_ID
+)
 
 apple_connector = AppleConnector(
     podcast_id=APPLE_PODCAST_ID,
