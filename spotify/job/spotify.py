4099c9fcdc630505b25630ef19e8c6b7509de464
Wolfgang Gassler <wolfgang@gassler.org>
2026-07-23 09:56:14 +0200

    Normalize Spotify performance responses (#141)

--- spotify/job/spotify.py
+++ spotify/job/spotify.py
@@ -2,6 +2,17 @@ from loguru import logger
 import datetime as dt
 
 
+def normalize_performance(data):
+    if not isinstance(data, dict) or not isinstance(data.get("samples"), list):
+        return data
+
+    return {
+        **data,
+        "sampleRate": data.get("sampleRate", 1000),
+        "seconds": data.get("seconds", len(data["samples"])),
+    }
+
+
 def get_episode_release_date(episode):
     """
     Returns the release date of an episode as a datetime object
